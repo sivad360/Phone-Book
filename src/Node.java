e92b6a1f66fb93ae5f6f8e3e5f5a471eac6af696
Davis Tyler <127260497+sivad360@users.noreply.github.com>
2024-03-15 16:44:24 -0700

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -2,7 +2,7 @@
 //Class: CS145
 //Date: 2/7/2024
 //Program: Phone Book
-//File: Test
+//File: Node
 //references: Lectures, https://www.devglan.com/datastructure/linkedlist-implementation-java
 //purpose: This program creates a linked list which is made up of people The program gives
 // the option to print out, search for, delete, or modify individuals
